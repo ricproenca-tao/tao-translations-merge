4ac6ae9296e1f9e0e9394b370777463fc8dbca7a
Ricardo <ricardo@taotesting.com>
2019-06-12 11:42:37 +0200

    initial files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tao-translations-merge
 Compare and merge customer translations
+
+This tool automate the merge of translations with *.PO files
---
+++ index.js
@@ -0,0 +1,6 @@
+const log = require('./src/utils/log.js');
+
+async function start() {
+    log.title('Base setup is working now!');
+}
+start();
