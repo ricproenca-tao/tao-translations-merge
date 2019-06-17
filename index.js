d7aeb95ec124d63604500fbac15b21d7fb99ba27
Ricardo <ricardo@taotesting.com>
2019-06-17 13:30:49 +0200

    improve final flow

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const log = require('./src/log');
-
-// TODO: set user, check tao release
 const runner = require('./src/runner')();
 
 (async function start() {
     try {
         log.title('TAO Merge Translations!');
         await runner.loadConfig();
-        await runner.selectTaoInstance();
 
+        await runner.selectTaoInstance();
         await runner.selectExtension();
         await runner.selectLanguage();
         await runner.selectTranslationsFolder();
         await runner.selectEmptyTranslationMode();
+
+        await runner.verifyLocalChanges();
         await runner.proceed();
 
-        // log.warn(JSON.stringify(runner.getData(), null, 2));
-
         await runner.prepareMerge();
-        await runner.proceedMerge();
-        await runner.mergeTranslations();
 
-        log.done('See you next time!');
+        await runner.createBranch();
+        await runner.mergeTranslations();
+        await runner.commitAndPush();
+
+        await runner.initialiseGithubClient();
+        await runner.createPullRequest();
+
+        log.done('Bye, bye! See you next time!');
     } catch (error) {
         log.error(error);
     }
